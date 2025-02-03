3a641e5e1c03bed47df5a32292f42d40da345657
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2025-02-03 16:20:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,12 @@ import styles from "./App.module.css";
 
 const products = [
   { id: 1, name: "Mobile 1", description: "Description 1", price: "$200" },
+  { id: 2, name: "Mobile 2", description: "Description 2", price: "$300" },
+  { id: 3, name: "Mobile 3", description: "Description 3", price: "$400" },
+  { id: 4, name: "Mobile 4", description: "Description 4", price: "$400" },
+  { id: 5, name: "Mobile 5", description: "Description 5", price: "$400" },
+  { id: 6, name: "Mobile 6", description: "Description 6", price: "$400" },
 ];
-
 
 function AdminProdcut() {
   const { id } = useParams();
